88490c782af8c85b640ad53d54896accc9bcc9b0
Anand Sinha <anandkumar.sinha@conduent.com>
2021-04-15 12:14:26 +0530

    Post and read message to/from the jms queue

--- src/main/java/com/example/weblogic/weblogicjms/poster/PostToQueue.java
+++ src/main/java/com/example/weblogic/weblogicjms/poster/PostToQueue.java
@@ -1,8 +1,86 @@
 package com.example.weblogic.weblogicjms.poster;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Hashtable;
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 public class PostToQueue {
 
-    public static void main(String args[]){
+    public final static String SERVER= "t3://localhost:7001";
+    public final static String JNDI_FACTORY= "weblogic.jndi.WLInitialContextFactory";
+    public final static String JMS_FACTORY= "/com/conduent/weblogic/base/cf";
+    public final static String QUEUE= "/com/conduent/weblogic/base/dq";
+
+    private QueueConnectionFactory queueConnectionFactory;
+    private QueueSession queueSession;
+    private QueueConnection queueConnection;
+    private QueueSender queueSender;
+    private Queue queue;
+    private TextMessage message;
+
+    public void init(Context context, String queueName) throws NamingException, JMSException {
+        queueConnectionFactory = (QueueConnectionFactory) context.lookup(JMS_FACTORY);
+        queue = (Queue) context.lookup(QUEUE);
+        queueConnection = queueConnectionFactory.createQueueConnection();
+        queueSession = queueConnection.createQueueSession(false,Session.AUTO_ACKNOWLEDGE);
+        queueSender = queueSession.createSender(queue);
+        message = queueSession.createTextMessage();
+        queueConnection.start();
     }
 
+    public void post(String msg) throws JMSException {
+        message.setText(msg);
+        queueSender.send(message);
+    }
+
+    public void close() throws JMSException {
+        queueSender.close();
+        queueSession.close();
+        queueConnection.close();
+    }
+
+    private static void sendToServer(PostToQueue queuePoster) throws IOException, JMSException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        boolean readFlag = true;
+        System.out.println("Sending JMS XML Content to WebLogic server");
+        while(readFlag) {
+        System.out.println("Enter Messages: ");
+        String msg = "Hey";
+        if(msg.equals("quit")){
+            queuePoster.post(msg);
+            System.exit(0);
+        }
+        queuePoster.post(msg);
+        System.out.println();
+        }
+            bufferedReader.close();
+    }
+
+    private static InitialContext getInitialContext() throws NamingException {
+
+        Hashtable<String, String> env = new Hashtable<String, String>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY,JNDI_FACTORY);
+        env.put(Context.PROVIDER_URL, SERVER);
+        return new InitialContext(env);
+    }
+
+    public static void main(String args[]) throws Exception {
+        InitialContext initialContext = getInitialContext();
+        PostToQueue queuePoster = new PostToQueue();
+        queuePoster.init(initialContext, QUEUE);
+        sendToServer(queuePoster);
+        System.out.print("\nMessage Successfully Sent to the JMS queue!!");
+        queuePoster.close();
+    }
 }
--- src/main/java/com/example/weblogic/weblogicjms/reader/ReadFromQueue.java
+++ src/main/java/com/example/weblogic/weblogicjms/reader/ReadFromQueue.java
@@ -1,7 +1,116 @@
 package com.example.weblogic.weblogicjms.reader;
 
-public class ReadFromQueue {
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.io.FileWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Hashtable;
 
-    public static void main(String args[]){
+public class ReadFromQueue implements MessageListener {
+
+    public final static String SERVER= "t3://localhost:7001";
+    public final static String JNDI_FACTORY= "weblogic.jndi.WLInitialContextFactory";
+    public final static String JMS_FACTORY= "/com/conduent/weblogic/base/cf";
+    public final static String QUEUE= "/com/conduent/weblogic/base/dq";
+
+    private QueueConnectionFactory queueConnectionFactory;
+    private QueueSession queueSession;
+    private QueueConnection queueConnection;
+    private QueueReceiver queueReceiver;
+    private Queue queue;
+    private Boolean quit = false;
+
+
+    public void init(Context context, String queueName) throws NamingException, JMSException {
+        queueConnectionFactory = (QueueConnectionFactory) context.lookup(JMS_FACTORY);
+        queue = (Queue) context.lookup(QUEUE);
+        queueConnection = queueConnectionFactory.createQueueConnection();
+        queueSession = queueConnection.createQueueSession(false,Session.AUTO_ACKNOWLEDGE);
+        queueReceiver = queueSession.createReceiver(queue);
+        queueReceiver.setMessageListener(this);
+        queueConnection.start();
+    }
+
+
+    public void close() throws JMSException {
+        queueReceiver.close();
+        queueSession.close();
+        queueConnection.close();
+    }
+
+
+    private static InitialContext getInitialContext() throws NamingException {
+		/*Properties p = new Properties();
+		p.put(Context.INITIAL_CONTEXT_FACTORY,"weblogic.jndi.WLInitialContextFactory");
+		p.put(Context.PROVIDER_URL, "t3://localhost:7001");*/
+
+        Hashtable<String, String> env = new Hashtable<String, String>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY,JNDI_FACTORY);
+        env.put(Context.PROVIDER_URL, SERVER);
+        return new InitialContext(env);
+    }
+
+    public static void main(String args[]) throws Exception {
+        System.out.println("Initiating...");
+        InitialContext initialContext = getInitialContext();
+        ReadFromQueue queueReader = new ReadFromQueue();
+        queueReader.init(initialContext, QUEUE);
+
+
+        synchronized (queueReader){
+            while (!queueReader.quit){
+                try {
+                    queueReader.wait();
+                } catch (InterruptedException ie){}
+                queueReader.close();
+
+            }
+        }
+
+
+        queueReader.close();
+        System.out.println("Finishing...");
+
+    }
+
+    @Override
+    public void onMessage(Message message) {
+
+        try {
+            String msgText;
+            if (message instanceof TextMessage){
+                msgText = ((TextMessage)message).getText();
+            } else {
+                msgText = message.toString();
+            }
+
+            try{
+                FileWriter fw=new FileWriter("JMS.xml");
+                fw.write(msgText);
+                fw.close();
+                System.out.println(msgText);
+//		        sftpOutBoundService.uploadToSftp(pipedFilePath);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+
+            if(msgText.equalsIgnoreCase("quit")){
+                synchronized (this){
+                    quit = true;
+                    this.notifyAll();
+                }
+            }
+        } catch(JMSException jmsException){
+            System.err.println("Exception: " + jmsException.getMessage());
+        } finally {
+
+        }
+
     }
 }
+
+
